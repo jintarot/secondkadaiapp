d41dff41c0a190d1505b4c561310b6210a1bbd45
jintaro.tanba <hnnskrhtnbsd2600@icloud.com>
2020-09-23 16:55:08 +0900

    Initial Commit

---
+++ second KadaiAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  second KadaiAPP
+//
+//  Created by HOUkoku on 2020/09/23.
+//  Copyright © 2020 jintaro.tanba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
